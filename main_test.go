f04ddec3682c9cec5772cc95c4ae5930b3af52cc
lucasfogagnoli <lucasfogagnoli@gmail.com>
2026-06-18 14:04:41 -0300

    Incluindo mais testes unitários e setando o modo de apresentação dos testes mais curto via gin.ReleaseMode

--- main_test.go
+++ main_test.go
@@ -10,12 +10,28 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/lucasfogagnoli/gin-api-rest/controllers"
+	"github.com/lucasfogagnoli/gin-api-rest/database"
+	"github.com/lucasfogagnoli/gin-api-rest/models"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "12345678901", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -38,4 +54,32 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	assert.Equal(t, mockDaResponse, string(responseBody))
 	fmt.Println(string(responseBody))
 	fmt.Println(mockDaResponse)
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaBD()
+
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConectaBD()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678901", nil)
+	response := httptest.NewRecorder()
+	r.ServeHTTP(response, req)
+
+	assert.Equal(t, http.StatusOK, response.Code)
 }
